2807633527b8a2b4285c648f5988a40f46fd964a
Austin Wise <AustinWise@gmail.com>
2021-01-18 12:32:57 -0800

    Add live reload support.

--- serve.sh
+++ serve.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $(dirname "$(readlink -f "$0")")
-bundle exec jekyll serve
+bundle exec jekyll serve -P 4000 --livereload --livereload-port 4001
--- serve_docker.cmd
+++ serve_docker.cmd
@@ -4,4 +4,4 @@ cd %~dp0
 
 docker build -t austinweb .
 if ERRORLEVEL 1 (EXIT /b 1)
-docker run -it --rm -p 4000:4000 -v %CD%:/app austinweb bundle exec jekyll serve --host=0.0.0.0 --force_polling
+docker run -it --rm -p 4000:4000 -p 4001:4001 -v %CD%:/app austinweb bundle exec jekyll serve --host=0.0.0.0 --force_polling -P 4000 --livereload --livereload-port 4001
