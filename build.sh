a5a8d5f58d104e3eeaeecd53c67183a4e00f75d9
Austin Wise <AustinWise@gmail.com>
2021-05-30 17:48:53 -0700

    Update Cobalt to v0.16.5

--- build.sh
+++ build.sh
@@ -5,9 +5,8 @@ set -x
 
 cd $(dirname "$(readlink -f "$0")")
 
-curl -o cobalt -SsL https://github.com/AustinWise/cobalt.rs/releases/download/austin-v1/cobalt
-
+curl -o cobalt.tar.gz -SsL  https://github.com/cobalt-org/cobalt.rs/releases/download/v0.16.5/cobalt-v0.16.5-x86_64-unknown-linux-gnu.tar.gz
 sha256sum -c cobalt_checksum.txt
+tar xf cobalt.tar.gz
 
-chmod +x ./cobalt
 ./cobalt build -c input/_cobalt.yml -d _site
--- serve.ps1
+++ serve.ps1
@@ -1,11 +1,11 @@
 $ProgressPreference = 'SilentlyContinue'
 if (!(Test-Path -Path .\cobalt.exe)) {
-    Write-Output "Downloading cobalt.exe"
-    Invoke-WebRequest -UseBasicParsing -OutFile cobalt.exe https://github.com/AustinWise/cobalt.rs/releases/download/austin-v1/cobalt.exe
-}
-$hash = Get-FileHash -Algorithm SHA256 .\cobalt.exe
-if ($hash.Hash -ne "3BECCD1EDE29D5C9F53DA05C99BA6988F2D52ED786EF7654DC9F07698CF4CA99") {
-    Remove-Item .\cobalt.exe
-    throw "failed to valdate hash"
+    Write-Output "Downloading cobalt"
+    Invoke-WebRequest -UseBasicParsing -OutFile cobalt.zip https://github.com/cobalt-org/cobalt.rs/releases/download/v0.16.5/cobalt-v0.16.5-x86_64-pc-windows-msvc.zip
+    $hash = Get-FileHash -Algorithm SHA256 .\cobalt.zip
+    if ($hash.Hash -ne "3CEA67F4A2BBB62E42B12ABAC29AA5C5842804E481EAF30E63B65C52975953B7") {
+        throw "failed to valdate hash"
+    }
+    Expand-Archive -Path .\cobalt.zip -DestinationPath .
 }
 & .\cobalt.exe serve -c input/_cobalt.yml -d _site
