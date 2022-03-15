b1d35a661d50fa64d840894c36a8eddac4595ee7
Austin Wise <AustinWise@gmail.com>
2022-03-14 20:32:18 -0700

    Add ability to build on macOS.

--- build.sh
+++ build.sh
@@ -5,8 +5,32 @@ set -x
 
 cd $(dirname "$(readlink -f "$0")")
 
-curl -o cobalt.tar.gz -SsL  https://github.com/cobalt-org/cobalt.rs/releases/download/v0.16.5/cobalt-v0.16.5-x86_64-unknown-linux-gnu.tar.gz
-sha256sum -c cobalt_checksum.txt
+version=v0.16.5
+
+os=$(uname -s | tr A-Z a-z) 
+vendor=unknown
+case $os in
+    darwin)
+        arch=x86_64
+        vendor=apple
+        ;;
+    linux)
+        os=linux-gnu
+        arch=$(uname -m)
+        ;;
+    *)
+        echo unknown OS: $os
+        exit 1
+        ;;
+esac
+
+curl -o cobalt.tar.gz -SsL  https://github.com/cobalt-org/cobalt.rs/releases/download/${version}/cobalt-${version}-${arch}-${vendor}-${os}.tar.gz
+# shasum -a 256 -c checksum-${os}-${arch}.txt
+if ! shasum -a 256 -c checksum-${os}-${arch}.txt ; then
+    echo actual SHA256:
+    shasum -a 256 cobalt.tar.gz
+    exit 1
+fi
 tar xf cobalt.tar.gz
 
 ./cobalt build -c input/_cobalt.yml -d _site
