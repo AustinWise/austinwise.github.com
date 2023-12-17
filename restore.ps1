872ff2efadbd654533e8ed6f2f9bd1ea9eebe71d
Austin Wise <AustinWise@gmail.com>
2023-12-17 14:48:00 -0800

    Update Cobalt to 0.19.0

--- restore.ps1
+++ restore.ps1
@@ -1,10 +1,10 @@
 $ProgressPreference = 'SilentlyContinue'
 if (!(Test-Path -Path .\cobalt\cobalt.exe)) {
     Write-Output "Downloading cobalt"
-    Invoke-WebRequest -UseBasicParsing -OutFile cobalt.zip https://github.com/cobalt-org/cobalt.rs/releases/download/v0.18.3/cobalt-v0.18.3-x86_64-pc-windows-msvc.zip
+    Invoke-WebRequest -UseBasicParsing -OutFile cobalt.zip https://github.com/cobalt-org/cobalt.rs/releases/download/v0.19.0/cobalt-v0.19.0-x86_64-pc-windows-msvc.zip
     $hash = Get-FileHash -Algorithm SHA256 .\cobalt.zip
-    if ($hash.Hash -ne "01F658867B03459D218CAC88FF18A4DFD836745A222239568A8AE7BADEF9A5E3") {
-        throw "failed to valdate hash"
+    if ($hash.Hash -ne "2170706C96F7685D83AD024B2CFE340CEA2B6558775C31C0A3DD6F93DEB794D2") {
+        throw "Unexpected hash: $hash"
     }
     if (-not (test-path cobalt)) {
         mkdir cobalt
